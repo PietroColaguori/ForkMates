5d27ac97af2607856d8c49792050524c7cd73243
PietroColaguori <pietro.colaguori3889@gmail.com>
2023-04-07 21:53:36 +0200

    Spostato in due cartelle "registration" e "login"
    Così è più compatto e non dobbiamo andarci a pescare le cose da "Pages" in cui sarebbe stato tutto insieme.

--- Main/Pages/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $authenticated = false;
-    if(isset($_POST['Verify']) && $_POST['verify'] == "Verify") {
-        if(!$authenticated) {
-            echo "Invalid credentials";
-        }
-    }
->
