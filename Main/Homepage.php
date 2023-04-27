48f9e44e7119236b28d0ccc47d12003b8bdd7164
PietroColaguori <pietro.colaguori3889@gmail.com>
2023-04-27 18:07:49 +0200

    Update and rename Homepage.html to Homepage.php

--- Main/Homepage.php
+++ Main/Homepage.php
@@ -12,42 +12,92 @@
      <link rel="stylesheet" href="css/pagefooter.css">
      <link rel="stylesheet" href="css/rating.css">
      <link rel="stylesheet" href="css/separator.css">
+     <link rel="stylesheet" href="css/bootstrap.min.css"> <!-- added -->
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script></head>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!-- MODAL DIALOG --> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<!-- NAVIGATION BAR-->
-<div class="topnav">
-    <img element id = "logo" src="icons/cooking.png"/> 
-    <a class="active" href="Homepage.html">ForkMates</a>
-    <a href="login/login.html">Sign In</a>
-    
-    <a href="registration/registration.html">Sign Up</a>
-    <a href="about.html">About</a>
-    <div class="search-container">
-        <form action="/ricerca_ricetta.php">
-          <input type="text" placeholder="Search a Recipe!" name="search" size="50">
-          <button type="submit">Search</button>
-        </form>
-      </div>
-</div>
-<br>
-<!--QUICK SEARCH BUTTONS-->
-<div id="Quick Search">
 
-  <!--PROVA DA ELIMINARE-->
-  <div class ="center">
-    <button onclick="window.location.href='Pages/login.html'" class="button" role = "button">Entries</button>
-    <button class="button" role="button">Main Dishes</button>
-    <button class="button" role="button">Second courses</button>
-    <button class="button" role="button">Desserts</button>
-  </div>
-  <div class = "center">  
-    <button class="button2">Your recipes of the day!</button>
+<?php
+  session_start();
+  error_reporting(E_ALL ^ E_WARNING);
+?>
+
+<!-- MODAL DIALOG -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">About ForkMates.com</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+      </div>
+      <div class="modal-body">
+        <p>This website was created by Pietro Colaguori and Claudio Cambone for the course "Languages and Technologies for the Web" at 
+        "La Sapienza" University of Rome. The purpose of the website is to enable its users to quickly and easily browse through
+        various recipes, to add their own recipes and to get every day new suggestions about what to eat!
+        If you want you can contact us to report a bug or make a donation!</p><br><hr>
+        <label class="form-label" for="donation">Make a donation! (1$ minimum)</label>
+        <input min="1" max="100" type="number" id="donation" class="form-control" /><br>
+        <button type="button" class="btn btn-success">Make Donation</button><br><br>
+        <label class="form-label" for="report">Report a bug!</label>
+        <textarea class="form-control" id="report" rows="3"></textarea><br>
+        <button type="button" class="btn btn-primary">Report</button>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
   </div>
 </div>
+
+
+<!-- NAVIGATION BAR-->
+
+<?php
+  if($_SESSION['loggedIn'] == true) {
+    echo "<div class='topnav'>
+        <img element id = 'logo' src='icons/cooking.png'/> 
+        <a class=\"active\" href=\"Homepage.php\">ForkMates</a>
+
+        <a href='add_recipe/add_recipe.html'>Add a recipe</a>
+        <a href='profile/profile.php'>Your Profile</a>
+        <a href=\"#\" data-toggle=\"modal\" data-target=\"#myModal\">Contact us</a>
+        <div class=\"search-container\">
+            <form action=\"/ricerca_ricetta.php\">
+              <input id=\"searchbar\" type=\"text\" placeholder=\"Search a Recipe!\" name=\"search\" size=\"50\" autocomplete=\"off\">
+              <button type=\"button\" class='btn btn-light'>Search</button>
+            </form>
+          </div>
+    </div>";
+  }
+  else {
+    echo "<div class='topnav'>
+        <img element id = 'logo' src='icons/cooking.png'/> 
+        <a class=\"active\" href=\"Homepage.html\">ForkMates</a>
+        <a href=\"login/login.html\">Sign In</a>
+        
+        <a href=\"registration/registration.html\">Sign Up</a>
+        <a href=\"#\" data-toggle=\"modal\" data-target=\"#myModal\">Contact us</a>
+        <div class=\"search-container\">
+            <form action=\"/ricerca_ricetta.php\">
+              <input id=\"searchbar\" type=\"text\" placeholder=\"Search a Recipe!\" name=\"search\" size=\"50\" autocomplete=\"off\">
+              <button type=\"submit\">Search</button>
+            </form>
+          </div>
+    </div>";
+  }
+?>
+
+<br>
  <!--RECIPE CARDS-->
  <div class="container">
     <div class="row">
@@ -58,7 +108,8 @@
                 <div id="card-title">Recipe of Porco Dio
                 </div>
                 <a href="recipe.html">
-                    <img element id ="recipe-image" src="icons/ricetta.jpeg">
+                    <div id="recipe-image" class="img-thumbnail">
+                    </div>
                 </a>
                 <div id="details">
                     <span class="detail-value">
